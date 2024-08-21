4d0f98f342e9aee431aa6e8a8f1b8db75286d2c0
maacofficial <maacep1@gmail.com>
2024-08-22 01:26:05 +0300

    Add new icon

---
+++ src/icons/exclamation-outline.tsx
@@ -0,0 +1,57 @@
+import React, { memo } from 'react';
+import type { IconProps } from '@tamagui/helpers-icon';
+import {
+  Svg,
+  Circle as _Circle,
+  G,
+  Path,
+  Text as _Text,
+  Circle,
+  ClipPath,
+  Defs,
+  Rect,
+  Text,
+  TSpan,
+} from 'react-native-svg';
+import { themed } from '@tamagui/helpers-icon';
+
+const Icon = (props) => {
+  const { color = 'black', size = 24, ...otherProps } = props
+  return (
+    <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 46 48"
+    {...otherProps}
+  >
+    <Text
+      id="_Title"
+      data-name="\u21B3 Title"
+      transform="translate(23 38)"
+      fill={color}
+      fontSize={size - 6}
+      fontFamily="Helvetica-Bold, Helvetica"
+      fontWeight={700}
+    >
+      <TSpan x={-6.993} y={0}>
+        {"!"}
+      </TSpan>
+    </Text>
+    <G
+      id="Ellipse_3611"
+      data-name="Ellipse 3611"
+      fill="none"
+      stroke={color}
+      strokeWidth={2}
+    >
+      <Circle cx={23} cy={23} r={23} stroke="none" />
+      <Circle cx={23} cy={23} r={22} fill="none" />
+    </G>
+  </Svg>
+  )
+}
+
+Icon.displayName = 'ExclamationOutline';
+
+export const ExclamationOutline = memo<IconProps>(themed(Icon));
